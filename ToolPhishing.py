e85f1bd71a7c589f8831f9889eb90787f42ea245
BlackBeliefSecurityTeam <70011454+BlackBeliefSecurityTeam@users.noreply.github.com>
2020-08-21 16:01:48 +0430

    Update ToolPhishing.py

--- ToolPhishing.py
+++ ToolPhishing.py
@@ -1,4 +1,18 @@
 # tool Phishing
+print"""
+\033[1;36m \n
+===============================
+#        Tool Phishing        #
+#  Black Belief Security Team #
+#  
+===============================
+
+
+[+]1:Zphisher
+[+]2:
+
+   
+"""
 o= input('Code=>')
 
 if o== 1:
